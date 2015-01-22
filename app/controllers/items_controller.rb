ed887979b63f7c736bd11925552ace16a23945b6
tevtonez <tevtonez@gmail.com>
2015-01-22 21:55:07 +0100

    redirecting to the lists#show page after editing/deletion the item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,7 +44,7 @@ class ItemsController < ApplicationController
   def update
     respond_to do |format|
       if @item.update(item_params)
-        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
+        format.html { redirect_to @item.list, notice: 'Item was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -58,7 +58,7 @@ class ItemsController < ApplicationController
   def destroy
     @item.destroy
     respond_to do |format|
-      format.html { redirect_to items_url }
+      format.html { redirect_to @item.list }
       format.json { head :no_content }
     end
   end
